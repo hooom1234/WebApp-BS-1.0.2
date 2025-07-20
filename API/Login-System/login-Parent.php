19ec3d95d3174a93e400d277d68d12ea03b6cabd
CatboiiTheCatboy <Supiranatpluem@outlook.com>
2025-07-20 21:45:00 +0700

    NEW Experimental Session Saving System

--- API/Login-System/login-Parent.php
+++ API/Login-System/login-Parent.php
@@ -31,6 +31,29 @@ $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     $_SESSION["parent_id"] = $parent_id; // เก็บ parent_id ใน session
+
+    // Session Saving System
+
+    $logInf = fopen("../../log.num", "w"); // creates a file
+    fwrite($logInf, $admin_id);
+    fclose($logInf);
+    /*$logInf = fopen("../../log.hs", "w"); // hashes the file
+    fwrite($logInf, hash("sha256", $admin_id));
+    fclose($logInf);*/
+    $logInf = fopen("../../log.typ", "w"); // creates another file, user type
+    fwrite($logInf, "p");
+    fclose($logInf);
+    /*$logInf = fopen("../../log.hst", "w"); // hashes the file
+    fwrite($logInf, hash("sha256", $admin_id));
+    fclose($logInf);*/
+    $logInf = fopen("../../log.tim", "w"); // creates a timestamp
+    fwrite($logInf, time());
+    fclose($logInf);
+    $logInf = fopen("../../log.key", "w"); // creates a thing
+    fwrite($logInf, $admin_password);
+    fclose($logInf);
+
+    
     echo json_encode(["status" => "success", "message" => "Login Success", "id" => $parent_id]);
 } else {
     echo json_encode(["status" => "error", "message" => "Invalid ID or Password"]);
--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -4,7 +4,8 @@ import ReporterButton from "./button/reporter-Button";
 import ParentButton from "./button/parent-Button";
 import AdminButton from "./button/admin-Button";
 
-import { useNavigation } from '@react-navigation/native';
+import RNFS from "react-native-fs";
+import axios from "axios";
 
 
 // ดึงขนาดหน้าจอ
@@ -87,5 +88,30 @@ const styles = StyleSheet.create({
     paddingVertical: height * 0.05, 
   },
 });
+
+//Session Checker
+
+const path = RNFS.DocumentDirectoryPath + '../../../';
+
+RNFS.readFile(path + 'log.tim', 'utf8')
+  .then(data => {
+    if (Math.floor(Date.now/1000) - +data > 1296000){ // user logged over 15 days ago
+      RNFS.unlink(path + 'log.tim').then().catch(err => console.log(err.message));
+      RNFS.unlink(path + 'log.typ').then().catch(err => console.log(err.message));
+      RNFS.unlink(path + 'log.num').then().catch(err => console.log(err.message));
+      RNFS.unlink(path + 'log.key').then().catch(err => console.log(err.message));
+      // removes files
+      return;
+    } else{ // user log is valid, restores session
+      const id = 0, key = 0;
+      RNFS.readFile(path + 'log.num').then(data => id  = data).catch(err => console.log(err.message));
+      RNFS.readFile(path + 'log.key').then(data => key = data).catch(err => console.log(err.message));
+      RNFS.readFile(path + 'log.typ').then(data => {
+        if (data == 'a') axios.post("${API_URL}Login-System/login-Admin.php", { id: id, etc });
+        if (data == 'p') axios.post("${API_URL}Login-System/login-Parent.php", { id: id, etc });
+        if (data == 'r') axios.post("${API_URL}Login-System/login-Reporter.php", { id: id, etc });
+      }).catch(err => console.log(err.message));
+    }
+  }).catch(err => console.log(err.message));
 
 export default HomeScreen;
